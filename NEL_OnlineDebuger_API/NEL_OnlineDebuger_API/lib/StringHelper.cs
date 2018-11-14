9035a8574ff17cb5af3fea24774082d8263f5878
tssqzr <1214661466@qq.com>
2018-11-14 10:03:03 +0800

    新增交易发送文件

---
+++ NEL_OnlineDebuger_API/NEL_OnlineDebuger_API/lib/StringHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEL_OnlineDebuger_API.lib
+{
+    public static class StringHelper
+    {
+        public static string Bytes2HexString(byte[] data)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var d in data)
+            {
+                sb.Append(d.ToString("x02"));
+            }
+            return sb.ToString();
+        }
+        public static byte[] HexString2Bytes(string str)
+        {
+            if (str.IndexOf("0x") == 0)
+                str = str.Substring(2);
+            byte[] outd = new byte[str.Length / 2];
+            for (var i = 0; i < str.Length / 2; i++)
+            {
+                outd[i] = byte.Parse(str.Substring(i * 2, 2), System.Globalization.NumberStyles.HexNumber);
+            }
+            return outd;
+        }
+    }
+}
